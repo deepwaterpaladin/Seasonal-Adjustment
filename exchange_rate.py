6b1e7931bf5b97fc63ab709c2437af147ea85607
deepwaterpaladin <rielly.young11@gmail.com>
2022-05-08 18:40:56 -0400

    class name changes

--- exchange_rate.py
+++ exchange_rate.py
@@ -51,7 +51,7 @@ class YUAN(ExchangeRate):
         self.ax.grid(True)
         self.ax.legend()
 
-class Rupee(ExchangeRate):
+class INR(ExchangeRate):
     '''
     Indian Rupee x Canadian Dollar
     '''
@@ -60,12 +60,31 @@ class Rupee(ExchangeRate):
         self.name = self.ws['D10'].value
         self.rupee_col = self.ws['D']
         self.rupee_db = pd.DataFrame({'Date': [i.value for i in self.date_col[13:1346]], 'Dollars': [i.value for i in self.rupee_col[13:1346]]})
-    def plot_rupee(self):
+    def plot_INR(self):
         self.fig, self.ax = plt.subplots(figsize=(18,9))
         self.ax.plot(self.rupee_db['Date'], self.rupee_db['Dollars'], label='1 INR')
         self.ax.set_title(self.name)
         self.ax.set_xlabel('Date')
         self.ax.set_ylabel('Dollars')
         self.ax.set_title('INR/CAD Exchange Rate')
+        self.ax.grid(True)
+        self.ax.legend()
+
+class RUB(ExchangeRate):
+    '''
+    Russian Ruble x Canadian Dollar
+    '''
+    def __init__(self):
+        super().__init__()
+        self.name = self.ws['E10'].value
+        self.RUB_col = self.ws['E']
+        self.RUB_db = pd.DataFrame({'Date': [i.value for i in self.date_col[13:1346]], 'Dollars': [i.value for i in self.RUB_col[13:1346]]})
+    def plot_RUB(self):
+        self.fig, self.ax = plt.subplots(figsize=(18,9))
+        self.ax.plot(self.RUB_db['Date'], self.RUB_db['Dollars'], label='1 RUB')
+        self.ax.set_title(self.name)
+        self.ax.set_xlabel('Date')
+        self.ax.set_ylabel('Dollars')
+        self.ax.set_title('RUB/CAD Exchange Rate')
         self.ax.grid(True)
         self.ax.legend()
